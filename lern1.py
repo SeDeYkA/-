fba5428f4effe0fc5b79c3929385b32794a07ebc
SeDeYkA <49610371+SeDeYkA@users.noreply.github.com>
2019-04-14 14:27:23 +0300

    Update lern1.py

--- lern1.py
+++ lern1.py
@@ -13,7 +13,6 @@ if name2 == '*':
     res = float(name1) * float(name3)
 print(f"Промежуточный результат: {res}")
 while name2 != '=':
-    name1 = input('введите число: ')
     name2 = input('что сделать? ')
     name3 = input('введите второе число: ')
     if name2 == '+':
@@ -21,8 +20,9 @@ while name2 != '=':
     if name2 == '-':
         res = res - float(name3)
     if name2 == '/':
-        if name2 != int('0'):
+        if name2 == int('0'):
             exit('на ноль не делят')
+        res = res / float(name3)
     if name2 == '*':
         res = res * float(name3)
     print(f"Промежуточный результат: {res}")
